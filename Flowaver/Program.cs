bdd3b2f1daf2f5abeb97b6d93c0d42234706f4c5
Hennico <nicolas_zanardo@hotmail.com>
2018-12-29 13:34:15 -0300

    Mover personaje

--- Flowaver/Program.cs
+++ Flowaver/Program.cs
@@ -13,15 +13,34 @@ namespace Flowaver
             Console.WindowWidth = tamX + 2;
             Console.WindowHeight = tamY + 3;
 
+            Plano<char> mapa = PlanoFactory<char>.GenerarLaberintoMirror(tamX, tamY, tamX / 2, tamY / 2, largo, ' ', '#');
+
+            ConsoleKey tecla = ConsoleKey.Pa1;
+            int[] posicion = new int[] { tamX / 2, tamY /2 };
+            mapa[posicion[0], posicion[1]] = new char[] { '@' };
             do
             {
+                switch (tecla)
+                {
+                    case ConsoleKey.UpArrow   : MoverPJ(mapa, posicion,  0, -1); break;
+                    case ConsoleKey.DownArrow : MoverPJ(mapa, posicion,  0, +1); break;
+                    case ConsoleKey.LeftArrow : MoverPJ(mapa, posicion, -1,  0); break;
+                    case ConsoleKey.RightArrow: MoverPJ(mapa, posicion, +1,  0); break;
+                }
+
                 Console.Clear();
-                //Plano<char> mapa = PlanoFactory<char>.GenerarLaberinto(tamX, tamY, tamX / 2, tamY / 2, largo, '·', '#');
-                Plano<char> mapa = PlanoFactory<char>.GenerarLaberintoMirror(tamX, tamY, tamX / 2, tamY / 2, largo, '·', '#');
-                mapa[tamX / 2, tamY / 2] = new char[] { '@' };
                 showMapa(mapa);
+                tecla = Console.ReadKey().Key;
             }
-            while (Console.ReadKey().Key != ConsoleKey.Spacebar);
+            while (tecla != ConsoleKey.Spacebar);
+        }
+
+        public static void MoverPJ(Plano<char> mapa, int[] posicion, int deltaX, int deltaY)
+        {
+            mapa[posicion[0], posicion[1]] = new char[] { ' ' };
+            posicion[0] += deltaX;
+            posicion[1] += deltaY;
+            mapa[posicion[0], posicion[1]] = new char[] { '@' };
         }
 
         public static void showMapa(Plano<char> mapa)
