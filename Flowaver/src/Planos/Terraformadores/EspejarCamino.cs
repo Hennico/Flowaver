0ca987a85aac7dcafd0c148458cd3005e06f275a
Hennico <nicolas_zanardo@hotmail.com>
2018-12-31 16:22:11 -0300

    corregir espejar para que tenga pivote dinamico

--- Flowaver/src/Planos/Terraformadores/EspejarCamino.cs
+++ Flowaver/src/Planos/Terraformadores/EspejarCamino.cs
@@ -15,14 +15,34 @@
 
         public void Terraformar(Plano<T> plano)
         {
-            for (int x = 0; x < plano.Ancho; x++)
+            int x,y;
+            int izq = plano.Ancho;
+            int der = 0;
+            int arr = plano.Alto;
+            int aba = 0;
+
+            for (x = 0; x < plano.Ancho; x++)
+            for (y = 0; y < plano.Alto; y++)
             {
-                for (int y = 0; y < plano.Alto; y++)
+                if (plano.LugarOcupado(x, y, piso))
+                {
+                    izq = izq > x ? x : izq;
+                    arr = arr > y ? y : arr;
+                    der = der < x ? x : der;
+                    aba = aba < y ? y : aba;
+                }
+            }
+            int xPibote = plano.Ancho / 2 > der ? der : (plano.Ancho / 2 < izq ? izq : plano.Ancho / 2);
+            int yPibote = plano.Alto  / 2 > aba ? aba : (plano.Alto  / 2 < arr ? arr : plano.Alto  / 2);
+            
+            for (x = 0; x < plano.Ancho; x++)
+            {
+                for (y = 0; y < plano.Alto; y++)
                 {
                     if (plano.LugarVacio(x, y))
                     {
-                        int xNew = horizontal ? plano.Ancho / 2 - (x - plano.Ancho / 2) : x;
-                        int yNew = vertical ? plano.Alto / 2 - (y - plano.Alto / 2) : y;
+                        int xNew = horizontal ? xPibote - (x - xPibote) : x;
+                        int yNew = vertical   ? yPibote - (y - yPibote) : y;
                         if (plano.LugarOcupado(xNew, yNew, piso))
                             plano.Agregar(x, y, piso);
                     }
