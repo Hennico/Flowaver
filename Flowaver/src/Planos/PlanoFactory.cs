3644068b4c230bced486743ed482e7afe43ad387
Hennico <nicolas_zanardo@hotmail.com>
2018-12-31 16:04:10 -0300

    modificar plano para que utilize terradormadores en lugar de codigo interno.

--- Flowaver/src/Planos/PlanoFactory.cs
+++ Flowaver/src/Planos/PlanoFactory.cs
@@ -4,149 +4,23 @@ namespace Flowaver.Planos
 {
     public static class PlanoFactory<T>
     {
-        public static Plano<T> GenerarLaberinto(int ancho, int alto, int xIni, int yIni, int largo, T piso, T pared)
+        public static Plano<T> GenerarLaberinto(int ancho, int alto, int xIni, int yIni, int largo, Random direcciones, T piso, T pared)
         {
             Plano<T> plano = new Plano<T>(ancho, alto);
-
-            Random direcciones = new Random();
-            int direccion = direcciones.Next(4);
-            int x = xIni;
-            int y = yIni;
-            for (int i = 0; i < largo / 2; i++)
-            {
-                plano.Agregar(x, y, piso);
-                x += deltaX(direccion);
-                y += deltaY(direccion);
-
-                if (!plano.PosicionValida(x + 2 * deltaX(direccion), y + 2 * deltaY(direccion)))
-                {
-                    direccion += 2;
-                    x += deltaX(direccion);
-                    y += deltaY(direccion);
-                }
-
-                plano.Agregar(x, y, piso);
-                x += deltaX(direccion);
-                y += deltaY(direccion);
-                direccion = direcciones.Next(4);
-            }
-
-            AgregarParedes(plano, piso, pared);
-            LimpiarColumnas(plano, piso, pared);
-
+            new CaminoLaberintico<T>(xIni, yIni, largo, piso, direcciones).Terraformar(plano);
+            new AmuradorDeCaminos<T>(piso, pared).Terraformar(plano);
+            new LimpiarColumnas<T>(piso, pared).Terraformar(plano);
             return plano;
         }
 
         public static Plano<T> GenerarLaberintoMirror(int ancho, int alto, int xIni, int yIni, int largo, Random direcciones, T piso, T pared)
         {
             Plano<T> plano = new Plano<T>(ancho, alto);
-
-            int direccion = direcciones.Next(4);
-            int x = xIni;
-            int y = yIni;
-            for (int i = 0; i < largo / 4; i++)
-            {
-                plano.Agregar(x, y, piso);
-                x += deltaX(direccion);
-                y += deltaY(direccion);
-
-                if (!plano.PosicionValida(x + 2 * deltaX(direccion), y + 2 * deltaY(direccion)))
-                {
-                    direccion += 2;
-                    x += deltaX(direccion);
-                    y += deltaY(direccion);
-                }
-
-                plano.Agregar(x, y, piso);
-                x += deltaX(direccion);
-                y += deltaY(direccion);
-                direccion = direcciones.Next(4);
-            }
-            CopiarEnEspejo(plano, piso, true, false);
-            CopiarEnEspejo(plano, piso, false, true);
-
-            AgregarParedes(plano, piso, pared);
-            //LimpiarColumnas(plano, piso, pared);
-
+            new CaminoLaberintico<T>(xIni, yIni, largo, piso, direcciones).Terraformar(plano);
+            new EspejarCamino<T>(piso, true, false).Terraformar(plano);
+            new EspejarCamino<T>(piso, false, true).Terraformar(plano);
+            new AmuradorDeCaminos<T>(piso, pared).Terraformar(plano);
             return plano;
-        }
-
-        private static void CopiarEnEspejo(Plano<T> plano, T piso, bool horizontal, bool vertical)
-        {
-            for (int x = 0; x < plano.Ancho; x++)
-            {
-                for (int y = 0; y < plano.Alto; y++)
-                {
-                    if (plano.LugarVacio(x, y))
-                    {
-                        int xNew = horizontal ? plano.Ancho / 2 - (x - plano.Ancho / 2) : x;
-                        int yNew = vertical ? plano.Alto / 2 - (y - plano.Alto / 2) : y;
-                        if (plano.LugarOcupado(xNew, yNew, piso))
-                            plano.Agregar(x, y, piso);
-                    }
-                }
-            }
-        }
-
-        private static void AgregarParedes(Plano<T> plano, T piso, T pared)
-        {
-            for (int x = 0; x < plano.Ancho; x++)
-            {
-                for (int y = 0; y < plano.Alto; y++)
-                {
-                    if (plano.LugarVacio(x, y))
-                    {
-                        bool conPiso = false;
-
-                        for (int varX = -1; varX < 2 && !conPiso; varX++)
-                            for (int varY = -1; varY < 2 && !conPiso; varY++)
-                                conPiso = plano.LugarOcupado(x + varX, y + varY, piso);
-
-                        if (conPiso)
-                            plano.Agregar(x, y, pared);
-                    }
-                }
-            }
-        }
-
-        private static void LimpiarColumnas(Plano<T> plano, T piso, T pared)
-        {
-            for (int x = 0; x < plano.Ancho; x++)
-            {
-                for (int y = 0; y < plano.Alto; y++)
-                {
-                    if (plano.LugarOcupado(x, y, pared))
-                    {
-                        bool todoPiso = true;
-
-                        for (int varX = -1; varX < 2 && todoPiso; varX++)
-                            for (int varY = -1; varY < 2 && todoPiso; varY++)
-                                todoPiso = varX == 0 && varY == 0 || plano.LugarOcupado(x + varX, y + varY, piso);
-
-                        if (todoPiso)
-                            plano[x, y] = new T[] { piso };
-                    }
-                }
-            }
-        }
-
-        private static int deltaX(int direccion)
-        {
-            direccion = direccion % 4;
-            if (direccion == 0)
-                return 1;
-            if (direccion == 2 || direccion == -2)
-                return -1;
-            return 0;
-        }
-        private static int deltaY(int direccion)
-        {
-            direccion = direccion % 4;
-            if (direccion == 1 || direccion == -3)
-                return 1;
-            if (direccion == 3 || direccion == -1)
-                return -1;
-            return 0;
         }
     }
 }
---
+++ Flowaver/src/Planos/Terraformadores/LimpiarColumnas.cs
@@ -0,0 +1,35 @@
+﻿namespace Flowaver.Planos
+{
+    class LimpiarColumnas<T> : ITerraformador<T>
+    {
+        private T piso;
+        private T pared;
+
+        public LimpiarColumnas(T piso, T pared)
+        {
+            this.piso = piso;
+            this.pared = pared;
+        }
+
+        public void Terraformar(Plano<T> plano)
+        {
+            for (int x = 0; x < plano.Ancho; x++)
+            {
+                for (int y = 0; y < plano.Alto; y++)
+                {
+                    if (plano.LugarOcupado(x, y, pared))
+                    {
+                        bool todoPiso = true;
+
+                        for (int varX = -1; varX < 2 && todoPiso; varX++)
+                            for (int varY = -1; varY < 2 && todoPiso; varY++)
+                                todoPiso = varX == 0 && varY == 0 || plano.LugarOcupado(x + varX, y + varY, piso);
+
+                        if (todoPiso)
+                            plano[x, y] = new T[] { piso };
+                    }
+                }
+            }
+        }
+    }
+}
